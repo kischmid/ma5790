8136668b86f7f885050752c968f1351a701f5388
kischmid <kischmid@mtu.edu>
2022-03-30 22:52:40 -0400

    Add non-linear models to modeling.R

--- modeling.R
+++ modeling.R
@@ -32,4 +32,50 @@ grade_test <- grade[-trainRows]
 
 ########## Non-Linear Models ##########
 
+## ----neural-net
+set.seed(802)
+nnetGrid <- expand.grid(.decay = c(0, 0.01, .1),
+                        .size = c(1:10),
+                        .bag = FALSE)
+nnetTune <- train(x=x_train, y=grade_train,
+                  method = "avNNet",
+                  tuneGrid = nnetGrid,
+                  trControl = trainControl(method="cv", number=10),
+                  linout = TRUE,
+                  trace = FALSE,
+                  MaxNWts = 10 * (ncol(x_train) + 1) + 10 + 1,
+                  maxit = 500,
+                  metric="Rsquared")
+print(nnetTune)
+plot(nnetTune)
+
+## ----mars
+set.seed(290)
+marsGrid <- expand.grid(degree = 1:3, nprune = 2:15)
+marsTrain <- train(x=x_train, y=grade_train, 
+                        method="earth",
+                        metric="Rsquared", 
+                        trControl=trainControl(method="cv", number=10), 
+                        tuneGrid=marsGrid)
+print(marsTrain)
+plot(marsTrain)
+
+## ----svm
+set.seed(625)
+svmTrain <- train(x=x_train, y=grade_train,
+                  method = "svmRadial",
+                  tuneLength = 10,
+                  metric="Rsquared",
+                  trControl = trainControl(method = "cv", number=10))
+print(svmTrain)
+ggplot(svmTrain)+coord_trans(x='log2')
+
+## ----knn
+set.seed(678)
+knnTrain <- train(x=x_train, y=grade_train, method="knn", 
+                  tuneLength=15, metric="Rsquared", 
+                  trControl=trainControl(method="cv", number=10))
+print(knnTrain)
+plot(knnTrain)
+
 
